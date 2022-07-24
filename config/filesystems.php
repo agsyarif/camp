a0daa71480d45fabf99ec93bdd381b9b5652d632
agsyarif <syrf553@gmail.com>
2022-07-24 12:29:33 +0700

    edit filesystems

--- config/filesystems.php
+++ config/filesystems.php
@@ -36,6 +36,13 @@ return [
             'throw' => false,
         ],
 
+        'hosting' => [
+            'driver' => 'local',
+            'root' => public_path() . '../../public_html/storage',
+            'url' => env('APP_URL') . '/storage',
+            'visibility' => 'public',
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
